8694ca3437304fbf2e8116c52be9905bc1010ef9
Franky Liang <fliangz96@gmail.com>
2017-08-28 13:30:13 -0400

    Create response for getting all listings

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var http = require('http'), 
-    fs = require('fs'), 
+var http = require('http'),
+    fs = require('fs'),
     url = require('url'),
     port = 8080;
 
@@ -8,19 +8,30 @@ var listingData, server;
 
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
-
-  /*
-    Your request handler should send listingData in the JSON format if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: explore the request object and its properties 
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-   */
+  if (request.method == 'GET') {
+    if (parsedUrl.pathname == '/listings') {
+      response.writeHead(200, {
+        'Content-Type': 'application/json'});
+      console.log(listingData)
+      response.end(listingData);
+    } else {
+      response.writeHead(404, {
+        'Content-Type': 'application/json'});
+      response.end("Bad gateway error");
+    }
+  } else {
+    response.writeHead(404, {
+      'Content-Type': 'application/json'});
+    response.end("Bad gateway error");
+  }
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+    This callback function should save the data in the listingData variable,
+    then start the server.
    */
+   listingData = data;
 });
+
+http.createServer(requestHandler).listen(port);
